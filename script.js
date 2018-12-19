984d67ca58f136b2f9bf404f9a6f7520965ed320
wycliff-paul <45225705+wycliff-paul@users.noreply.github.com>
2018-12-19 08:24:28 +0300

    changes made

---
+++ script.js
@@ -0,0 +1,7 @@
+//business logic
+var players = function(){
+var player1 = parseInt(getElementById('player1').value());
+var player2 = parseInt(getElementById('player2').value());
+//Check if both players have the same turn and roll.
+
+if(player1 >= 100 || pla)
